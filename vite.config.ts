db3eeb19de82ca86c6555a42d75b85814fd72978
pradnya701 <65855910+pradnya701@users.noreply.github.com>
2026-02-23 10:53:19 -0500

    Change base path in Vite config to '/macos/'

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/", // 👈 ADD THIS LINE
+  base: "/macos/",  // 👈 Must match your repo name
   plugins: [
     unocss(),
     react(),
